c125086079c04680b7a628856c71ea03f083cf1c
sdnall <s.daniel.nall@gmail.com>
2010-10-13 14:59:09 -0400

    add maintenance mode tasks to drupal's cap script, when deploying the site will be put into maintenance mode before deploying, and taken out when the deploy finishes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,14 +19,14 @@ on :load, "deploy:setup_environment"
 
 # Disable our app before running the deploy
 #TODO: either figure out how to use drush to put drupal into maintenance mode
-#before "deploy", "deploy:web:disable"
+before "deploy", "deploy:web:disable"
 
 # After code is updated, do some house cleaning
 after "deploy:update_code", "deploy:link_configs"
 after "deploy:update_code", "deploy:cleanup"
 
 # don't forget to turn it back on
-#after "deploy", "deploy:web:enable"
+after "deploy", "deploy:web:enable"
 after "deploy", 'deploy:notification:email'
 
 # Add tasks to the deploy namespace
@@ -67,14 +67,14 @@ namespace :deploy do
     # Override default web enable/disable tasks
   namespace :web do
     
-    desc "Put Apache in maintenancemode by touching the system/maintenancemode file"
+    desc "Put Drupal in maintenancemode by updating the maintenancemode db variable"
     task :disable, :roles => :app do
-      invoke_command "touch #{shared_path}/system/maintenancemode"
+      invoke_command "/usr/local/bin/drush -r #{current_path} vset --always-set maintenance_mode 1"
     end
   
     desc "Remove Apache from maintenancemode by removing the system/maintenancemode file"
     task :enable, :roles => :app do
-      invoke_command "rm -f #{shared_path}/system/maintenancemode"
+      invoke_command "/usr/local/bin/drush -r #{current_path} vset --always-set maintenance_mode 0"
     end
     
   end
