e8fb9591503389ec72f109e09dc8f8a8a2db7249
Ivelin Denev <idengo@gmail.com>
2010-11-09 13:55:05 -0500

    including module field_group which can be used to group fields in content types. this can be particullary useful in including numerous fields as a metadata which can turn to long, and scary list for the users.

---
+++ sites/all/modules/field_group/field_group.js
@@ -0,0 +1,83 @@
+// $Id: field_group.js,v 1.5.2.8 2010/11/08 21:30:56 stalski Exp $
+
+(function($) {
+
+/**
+ * Drupal FieldGroup object.
+ */
+Drupal.FieldGroup = Drupal.FieldGroup || {};
+Drupal.FieldGroup.Effects = Drupal.FieldGroup.Effects || {};
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processAccordion = {
+  execute: function (context, settings) {
+    $('div.field-group-accordion-wrapper', context).accordion({ autoHeight: false });
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processHtabs = {
+  execute: function (context, settings) {
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ * 
+ * TODO clean this up meaning check if this is really 
+ *      necessary.
+ */
+Drupal.FieldGroup.Effects.processDiv = {
+  execute: function (context, settings) {
+
+    $('div.collapsible', context).each(function() {
+      var $wrapper = $(this);
+
+      // Turn the legend into a clickable link, but retain span.field-group-format-toggler
+      // for CSS positioning.
+      var $toggler = $('span.field-group-format-toggler:first', $wrapper);
+      var $link = $('<a class="field-group-format-title" href="#"></a>');
+      $link.prepend($toggler.contents()).appendTo($toggler);
+      
+      // .wrapInner() does not retain bound events.
+      $link.click(function () {
+        var wrapper = $wrapper.get(0);
+        // Don't animate multiple times.
+        if (!wrapper.animating) {
+          wrapper.animating = true;
+          $('> .field-group-format-wrapper', wrapper).toggle('blind', {}, 500);
+          wrapper.animating = false;
+        }
+        return false;
+      });
+      
+    });
+  }
+};
+
+/**
+ * Behaviors.
+ */
+Drupal.behaviors.fieldGroup = {
+  attach: function (context, settings) {
+    if (settings.field_group == undefined || settings.fieldGroupWrapper == undefined) {
+      return;
+    }
+    $('#' + settings.fieldGroupWrapper, context).once('fieldgroup-effects', function () {
+      // Execute all of them.
+      $.each(Drupal.FieldGroup.Effects, function (func) {
+        // We check for a wrapper function in Drupal.field_group as 
+        // alternative for dynamic string function calls.
+        if (settings.field_group[func.toLowerCase().replace("process", "")] != undefined && $.isFunction(this.execute)) {
+          this.execute(context, settings);
+        }
+      });
+    });
+  }
+};
+
+})(jQuery);
