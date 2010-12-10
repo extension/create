8701b32de0f274e12c3cd7c3f0cc320ea17126e2
athundle <aaron.hundley@extension.org>
2010-12-10 14:25:46 -0500

    More work on listviews. Fixed an issue with pagination, give list view in a tabular format, make 'id' and 'created' column headers sortable and make it work with pagination. Also made modifications to database query and a fixed a few issues there. Still in test stages and code needs to eventually be refactored.

--- sites/all/modules/exworkflow/exworkflow_draft.tpl.php
+++ sites/all/modules/exworkflow/exworkflow_draft.tpl.php
@@ -1,5 +1,32 @@
+<?php print $pager; ?>
 
+<table>
+
+  <tr>
+      <th>
+          <?php print l('Id', $_GET['q'], 
+                      array('query' => array('field' => 'nid') + $sort_direction + $page_number)); 
+          ?>
+      </th>
+      
+      <th>Title</th>
+      
+      <th>
+          <?php print l('Created', $_GET['q'], 
+                      array('query' => array('field' => 'created') + $sort_direction + $page_number)); 
+          ?>
+      </th>
+  </tr>
+  
 <?php foreach ($draft_nodes as $draft_node): ?>
-  <p><?php print $draft_node->title ?></p>
+  <tr>
+    <td><?php print $draft_node->nid; ?></td>
+    <td><?php print l($draft_node->title, "/node/{$draft_node->nid}"); ?></td>
+    <td><?php print format_date($draft_node->created); ?></td>
+  </tr>
 <?php endforeach; ?>
+  
+</table>
+
+
 <?php print $pager; ?>
