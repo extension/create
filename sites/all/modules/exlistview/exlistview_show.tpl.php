7924854ef04f4a4986b963d2adb5766b9543c568
athundle <aaron.hundley@extension.org>
2011-01-05 16:56:21 -0500

    All workflow states available now in listviews and added menu options for various workflow states (still gonna work on making that cleaner), made code more flexible and cleaner for listviews along with correcting a bug in the view code.

--- sites/all/modules/exlistview/exlistview_show.tpl.php
+++ sites/all/modules/exlistview/exlistview_show.tpl.php
@@ -1,8 +1,14 @@
 <?php print drupal_render($tag_form) ?>
 
+<?php $more_than_zero_nodes = FALSE; ?>
 
-<?php if (count($draft_nodes) == 0): ?>
-<p>No content available with specified tags.</p>
+<?php foreach ($listview_nodes as $listview_node): ?>
+  <?php $more_than_zero_nodes = TRUE; ?>
+  <? break; ?>
+<?php endforeach; ?>  
+
+<?php if (!$more_than_zero_nodes): ?>
+<p>No content available with specified tags and workflow states.</p>
 <?php else: ?>
 
 <?php print $pager; ?>
@@ -25,11 +31,11 @@
       </th>
   </tr>
   
-<?php foreach ($draft_nodes as $draft_node): ?>
+<?php foreach ($listview_nodes as $listview_node): ?>
   <tr>
-    <td><?php print $draft_node->nid; ?></td>
-    <td><?php print l($draft_node->title, "/node/{$draft_node->nid}"); ?></td>
-    <td><?php print format_date($draft_node->created); ?></td>
+    <td><?php print $listview_node->nid; ?></td>
+    <td><?php print l($listview_node->title, "/node/{$listview_node->nid}"); ?></td>
+    <td><?php print format_date($listview_node->created); ?></td>
   </tr>
 <?php endforeach; ?>
   
