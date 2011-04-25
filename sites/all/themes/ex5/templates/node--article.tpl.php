77b4a9b7d2b444f5bb5eefb375db4dcb43c93748
Ivelin Denev <idengo@gmail.com>
2011-04-25 12:44:10 -0400

    reducing confusion of existence of the workflow controls in the presentation part of the articles by moving those controls bellow actual content

---
+++ sites/all/themes/ex5/templates/node--article.tpl.php
@@ -0,0 +1,33 @@
+<article id="article-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix" role="article"<?php print $attributes; ?>>
+  <?php print $unpublished; ?>
+  <?php if ($title && !$page): ?>
+    <header>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1<?php print $title_attributes; ?>>
+          <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
+        </h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+    </header>
+  <?php endif; ?>
+  <?php if ($display_submitted): ?>
+    <footer class="submitted">
+      <?php //print $user_picture; ?>
+      <?php print $updated; ?>
+    </footer>
+  <?php endif; ?>
+  <div<?php print $content_attributes; ?>>
+    <?php
+      hide($content['comments']);
+      hide($content['links']);
+	  hide($content['additional_field']);
+      print render($content);
+    ?>
+  </div>
+  <div class="clearfix">
+  <?php print render($content['additional_field']); ?>
+  </div>
+  <?php if ($links = render($content['links'])): print $links; endif; ?>
+  <?php print render($content['comments']); ?>
+</article>
