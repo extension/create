a65ff423169cb574a4a60912048e80e8c3155f6e
Ivelin Denev <idengo@gmail.com>
2011-05-12 11:36:53 -0400

    because views module seems to not be able to filter fields stored as timestamp, the field for time in event content type is changed to datetime. the views template is modified in order to reflect this change.

--- sites/all/themes/ex5/templates/views-view-field--events--page--field-event-time.tpl.php
+++ sites/all/themes/ex5/templates/views-view-field--events--page--field-event-time.tpl.php
@@ -21,10 +21,9 @@
 ?>
 <?php
 
-print date('d', $row->field_data_field_event_time_field_event_time_value);
 
+$date_created = DateTime::createFromFormat('Y-m-d H:i:s', $row->field_data_field_event_time_field_event_time_value);
+print $date_created->format('d');
 
-
- //print $output; 
  
  ?>
