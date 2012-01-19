3d7de9cdf52e80977720ac4742d99582a05b2d68
Ivelin Denev <idengo@gmail.com>
2012-01-19 12:13:00 -0500

    modules update

---
+++ sites/all/modules/media_update/media_update.api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Hooks provided by the media_update module.
+ */
+
+/**
+ * Allows other modules to be notified when a user
+ * updates an existing media file with a new file.
+ *
+ * @param $file
+ *   The original file object from before the update.
+ */
+function hook_media_update_updated($file) {
+
+}
