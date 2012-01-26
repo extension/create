469d30e72f36cfee3296152482a57b970080abfb
Ivelin Denev <idengo@gmail.com>
2012-01-26 14:39:43 -0500

    fixing a problem in appraisal fields created by update of the Date module

--- sites/all/themes/ex5/templates/node--optimization.tpl.php
+++ sites/all/themes/ex5/templates/node--optimization.tpl.php
@@ -1,5 +1,5 @@
 <?php
-
+drupal_add_js('misc/collapse.js');
 $structure = array(
 
 	array( 	'section'=> 'Characteristics', 
@@ -109,7 +109,94 @@ $structure = array(
 
  $gr = og_load($content['group_audience']['#items'][0]['gid']);
  
+ 
+ //$fld = field_info_instance('node', 'field_animations_ranking', 'optimization');
+ 
+ 
+ //dsm($fld);
 ?>
+
+
+
+<fieldset class="search-advanced collapsible collapsed form-wrapper" id="edit-advanced"><legend><span class="fieldset-legend">Advanced search</span></legend><div class="fieldset-wrapper"><div class="criterion"><div class="form-item form-type-textfield form-item-or">
+  <label for="edit-or">Containing any of the words </label>
+ <input type="text" id="edit-or" name="or" value="" size="30" maxlength="255" class="form-text" />
+</div>
+<div class="form-item form-type-textfield form-item-phrase">
+  <label for="edit-phrase">Containing the phrase </label>
+ <input type="text" id="edit-phrase" name="phrase" value="" size="30" maxlength="255" class="form-text" />
+</div>
+<div class="form-item form-type-textfield form-item-negative">
+
+  <label for="edit-negative">Containing none of the words </label>
+ <input type="text" id="edit-negative" name="negative" value="" size="30" maxlength="255" class="form-text" />
+</div>
+</div><div class="criterion"><div class="form-item form-type-checkboxes form-item-type">
+  <label for="edit-type">Only of the type(s) </label>
+ <div id="edit-type" class="form-checkboxes"><div class="form-item form-type-checkbox form-item-type-article">
+ <input type="checkbox" id="edit-type-article" name="type[article]" value="article" class="form-checkbox" />  <label class="option" for="edit-type-article">Article </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-page">
+ <input type="checkbox" id="edit-type-page" name="type[page]" value="page" class="form-checkbox" />  <label class="option" for="edit-type-page">Basic page </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-faq">
+ <input type="checkbox" id="edit-type-faq" name="type[faq]" value="faq" class="form-checkbox" />  <label class="option" for="edit-type-faq">FAQ </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-feed-items">
+ <input type="checkbox" id="edit-type-feed-items" name="type[feed_items]" value="feed_items" class="form-checkbox" />  <label class="option" for="edit-type-feed-items">Feed Items </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-feeds">
+ <input type="checkbox" id="edit-type-feeds" name="type[feeds]" value="feeds" class="form-checkbox" />  <label class="option" for="edit-type-feeds">Feeds </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-forum">
+ <input type="checkbox" id="edit-type-forum" name="type[forum]" value="forum" class="form-checkbox" />  <label class="option" for="edit-type-forum">Forum topic </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-group">
+
+ <input type="checkbox" id="edit-type-group" name="type[group]" value="group" class="form-checkbox" />  <label class="option" for="edit-type-group">Group </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-cop-document">
+ <input type="checkbox" id="edit-type-cop-document" name="type[cop_document]" value="cop_document" class="form-checkbox" />  <label class="option" for="edit-type-cop-document">Group Admin Document </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-event">
+ <input type="checkbox" id="edit-type-event" name="type[event]" value="event" class="form-checkbox" />  <label class="option" for="edit-type-event">Group Event </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-blog">
+ <input type="checkbox" id="edit-type-blog" name="type[blog]" value="blog" class="form-checkbox" />  <label class="option" for="edit-type-blog">Group News </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-help-resource">
+ <input type="checkbox" id="edit-type-help-resource" name="type[help_resource]" value="help_resource" class="form-checkbox" />  <label class="option" for="edit-type-help-resource">Help resource </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-news">
+
+ <input type="checkbox" id="edit-type-news" name="type[news]" value="news" class="form-checkbox" />  <label class="option" for="edit-type-news">News </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-optimization">
+ <input type="checkbox" id="edit-type-optimization" name="type[optimization]" value="optimization" class="form-checkbox" />  <label class="option" for="edit-type-optimization">Optimization Assessment Scorecard </label>
+
+</div>
+<div class="form-item form-type-checkbox form-item-type-public-article">
+ <input type="checkbox" id="edit-type-public-article" name="type[public_article]" value="public_article" class="form-checkbox" />  <label class="option" for="edit-type-public-article">eXtension Admin Document </label>
+
+</div>
+</div>
+</div>
+</div><div class="action"><input type="submit" id="edit-submit--2" name="op" value="Advanced search" class="form-submit" /></div></div></fieldset>
+
+
+
 
 <article id="article-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix" role="article"<?php print $attributes; ?>>
 
@@ -147,10 +234,17 @@ $structure = array(
 	  	print '<section>
 		<h4>'.$sections['section'].'</h4>';
 		foreach($sections['elements'] as $element){
+			$fld = field_info_instance('node', $element['ranking'], 'optimization');
 			print '<div class="scorecard_field">';
-			print '	<div class="scorecard_ranking">'.$content[$element['ranking']]['#title'];
-			print ': <span class="scorecard_score">'.$content[$element['ranking']]['#items'][0]['value'].'			</span>
-			</div>';
+			print '
+			
+			<fieldset class="collapsible collapsed">
+			
+			
+				<legend>';
+			print '<span class="scorecard_score fieldset-legend">'.$content[$element['ranking']]['#title'].$content[$element['ranking']]['#items'][0]['value'].'			</span></legend>';
+			print ''.$fld['description'].'
+			</fieldset>';
 			print '<div class="scorecard_note">'.$content[$element['note']]['#items'][0]['safe_value'].'</div>';
 			print '</div>';
 			$sum += $content[$element['ranking']]['#items'][0]['value'];
