0605d7fb42108b2661dd9e90a0850c79b59cff5e
sdnall <s.daniel.nall@gmail.com>
2013-03-19 09:44:03 -0400

    get rid of delayed/recipes require. we dont need none of that.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ set :default_stage, "demo"
 require 'capistrano/ext/multistage'
 
 require 'capatross'
-require "delayed/recipes"
  
 set :application, "drupal"
 set :repository,  "git@github.com:extension/drupal.git"
