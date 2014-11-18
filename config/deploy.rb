e7a35f2b5fbbeffb8176d416bdd856b781f63882
Jason Adam Young <jayoung@extension.org>
2014-11-18 15:56:09 -0500

    deployment changes for trusty

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
-set :stages, %w(prod dev bootcamp)
+set :stages, %w(prod dev)
 set :default_stage, "dev"
 require 'capistrano/ext/multistage'
 
 require 'capatross'
- 
-set :application, "drupal"
+
+set :application, "create"
 set :repository,  "git@github.com:extension/drupal.git"
 set :scm, "git"
 set :user, "pacecar"
@@ -16,22 +16,55 @@ set :port, 24
 
 after "deploy:update_code", "deploy:link_and_copy_configs"
 after "deploy:update_code", "deploy:cleanup"
+after 'deploy',             'deploy:cacheclear'
+after 'deploy',             'deploy:restart'
 
 
 namespace :deploy do
-  
-  
+
+   # Override default restart task
+   desc "Restart Apache"
+   task :restart, :roles => :app do
+     invoke_command '/usr/sbin/service apache2 restart', via: 'sudo'
+   end
+
+   # clear cache
+   desc "Drush Cache Clear"
+   task :cacheclear, :roles => :app do
+     execute "cd #{release_path} && /usr/bin/drush cache-clear all"
+   end
+
   # Link up various configs (valid after an update code invocation)
   task :link_and_copy_configs, :roles => :app do
     run <<-CMD
     rm -rf #{release_path}/sites/default/settings.php &&
     rm -rf #{release_path}/sites/default/files &&
     rm #{release_path}/robots.txt &&
-    ln -nfs /services/config/#{application}/robots.txt #{release_path}/robots.txt &&
-    ln -nfs /services/config/#{application}/settings.php #{release_path}/sites/default/settings.php &&
-    ln -nfs #{shared_path}/files #{release_path}/sites/default/files
+    ln -nfs /services/#{application}/shared/config/robots.txt #{release_path}/robots.txt &&
+    ln -nfs /services/#{application}/shared/config/settings.php #{release_path}/sites/default/settings.php &&
+    ln -nfs /services/nfs/drupalfiles/files #{release_path}/sites/default/files
     CMD
   end
 
-end
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with this application"
+    task t, :roles => :app do ; end
+  end
 
+
+   # Override default web enable/disable tasks
+   namespace :web do
+
+      desc "Put Apache in maintenancemode by touching the maintenancemode file"
+      task :disable, :roles => :app do
+        invoke_command "touch /services/maintenance/#{vhost}.maintenancemode"
+      end
+
+      desc "Remove Apache from maintenancemode by removing the maintenancemode file"
+      task :enable, :roles => :app do
+        invoke_command "rm -f /services/maintenance/#{vhost}.maintenancemode"
+      end
+
+   end
+
+end
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,7 +1,8 @@
-set :deploy_to, '/services/apache/vhosts/dev.create.extension.org/docroot/'
-server 'dev.create.extension.org', :app, :web, :db, :primary => true
+set :deploy_to, "/services/create/"
 if(branch = ENV['BRANCH'])
   set :branch, branch
 else
   set :branch, 'develop'
 end
+set :vhost, 'dev-create.extension.org'
+server 'chowanswamp.vm.extension.org', :app, :web, :db, :primary => true
